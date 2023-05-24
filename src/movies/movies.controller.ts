de85f45aff1167c2024c2cab109dc4e7c586cc98
SR-Jung <westeeven0612@gmail.com>
2023-05-24 15:58:57 +0900

    express-on-nestJS

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -2,7 +2,7 @@ import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdataeMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/Movie.entity';
 import { MoviesService } from './movies.service';
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Req, Res } from '@nestjs/common';
 
 @Controller('movies') //''<-endpoint
 export class MoviesController {
@@ -12,7 +12,7 @@ export class MoviesController {
 
   @Get()
   getAll(): Movie[] {
-    // return "This will return all movies";
+    // return "This will return all movies"; 
     return this.MoviesService.getAll();
   }
 
